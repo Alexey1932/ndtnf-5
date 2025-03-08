fe78619f13e400f0ad781a4266e19a5eebc278f5
AlexAnt <grauheim.today@gmail.com>
2025-03-08 07:52:30 +0300

    Initial commit

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -6,26 +6,50 @@ import { CreateBookDto } from '../interfaces/dto/create_book.dto';
 
 @Injectable()
 export class BooksService {
-  constructor(@InjectModel(Book.name) private BookModel: Model<BookDocument>) {}
+	constructor(@InjectModel(Book.name) private BookModel: Model<BookDocument>) { }
 
-  async create(create_book: CreateBookDto): Promise<BookDocument> {
-    const book = new this.BookModel(create_book);
-    return book.save();
-  }
+	async create(create_book: CreateBookDto): Promise<BookDocument> {
+		const book = new this.BookModel(create_book);
+		return book.save();
+	}
 
-  async findAll(): Promise<BookDocument[]> {
-    return this.BookModel.find().exec();
-  }
+	async findAll(): Promise<BookDocument[]> {
+		try {
+			const books = await this.BookModel.find().exec();
+			if (!books || books.length === 0) {
+				throw new Error('Книги не найдены');
+			}
+			return books;
+		} catch (error) {
+			throw new Error(`Ошибка при получении списка книг: ${error.message}`);
+		}
+	}
 
-  async getBook(id: string): Promise<BookDocument> {
-    return this.BookModel.findById({ _id: id }).exec();
-  }
+	async getBook(id: string): Promise<BookDocument> {
+		return this.BookModel.findById({ _id: id }).exec();
+	}
 
-  async deleteBook(id: string): Promise<BookDocument> {
-    return this.BookModel.findOneAndRemove({ _id: id }).exec();
-  }
+	async deleteBook(id: string): Promise<BookDocument> {
+		try {
+			const deletedBook = await this.BookModel.findOneAndRemove({ _id: id }).exec();
+			if (!deletedBook) {
+				throw new Error('Книга не найдена');
+			}
+			return deletedBook;
+		} catch (error) {
+			throw new Error(`Ошибка при удалении книги: ${error.message}`);
+		}
+	}
 
-  async updateBook(id: string, data: CreateBookDto): Promise<BookDocument> {
-    return this.BookModel.findOneAndUpdate({ _id: id }, data).exec();
-  }
+	async updateBook(id: string, data: CreateBookDto): Promise<BookDocument> {
+		try {
+		  const updatedBook = await this.BookModel.findOneAndUpdate({ _id: id }, data, { new: true }).exec();
+		  if (!updatedBook) {
+			 throw new Error('Книга не найдена');
+		  }
+		  return updatedBook;
+		} catch (error) {
+		  throw new Error(`Ошибка при обновлении книги: ${error.message}`);
+		}
+	 }
 }
